27d0a7035b2ba55177a3c77d46503c488df80f7b
zhukowych <mzmzhuk@gmail.com>
2025-04-09 02:36:59 +0300

    updated test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import itertools as it
 import matplotlib.pyplot as plt
 from uot.dataset import generate_1d_gaussians_ds, generate_2d_gaussians_ds, generate_3d_gaussians_ds
 from algorithms.sinkhorn import sinkhorn
-from uot.experiment import OTProblem, Experiment, get_exact_solution, generate_two_fold_problems 
+from uot.experiment import OTProblem, Experiment, ExperimentSuite, generate_two_fold_problems 
 
 
 x = np.linspace(-10, 10, 100)
@@ -12,19 +12,19 @@ x, y = np.meshgrid(x, y)
 
 
 gaussians_2d = generate_2d_gaussians_ds(x, y)
-
 ot_problems = generate_two_fold_problems([x, y], gaussians_2d)
 
 
-# def conduct_precisoin_experiment(ot_problem: OTProblem):
-#     a, b, C = ot_problem.a, ot_problem.b, ot_problem.C
-#     _, exact_dist = get_exact_solution(a, b, C)
+def conduct_precisoin_experiment(ot_problem: OTProblem):
+    return {'precision': np.random.randn()}
 
-#     sinkhorn_coupling = sinkhorn(a, b, C) 
-#     sinkhorn_distance = np.sum(C * sinkhorn_coupling)    
-#     precision = np.abs(sinkhorn_distance - exact_dist) / exact_dist
-#     return {'precision': precision}
+def conduct_dummy_experiment(ot_problem: OTProblem):
+    return {'dummy_variable': 1}
 
+ot_problems[0].kwargs = {'eps': 1}
 
-# experiment = Experiment("Sinkhorn precision test", ot_problems, run_function=conduct_precisoin_experiment)
-# results = experiment.run_experiment()
+experiment = Experiment("Sinkhorn precision test", run_function=conduct_precisoin_experiment)
+second_expetiment = Experiment("Dummy experiment", run_function=conduct_dummy_experiment)
+experiment_suite = ExperimentSuite([experiment, second_expetiment])
+results = experiment_suite.run_suite(ot_problems, njobs=2)
+print(results)
