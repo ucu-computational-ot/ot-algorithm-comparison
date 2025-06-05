564b59bf7e5402f5761c6aab10945572a89340fc
Ivan <aipyth.func@gmail.com>
2025-06-04 23:42:16 -0400

    feature: basic logger

---
+++ uot/utils/logging.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def setup_logger(
+        name: str = "uot",
+        level: int = logging.INFO
+) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        fmt = logging.Formatter(
+            "%(asctime)s %(name)s %(levelname)s: %(message)s")
+        ch.setFormatter(fmt)
+        logger.addHandler(ch)
+    logger.setLevel(level)
+    return logger
+
+
+logger = setup_logger()
