0ba0ac98fb305b32dd0c850451dfa86eaa1f0744
Ivan Zhytkevych <aipyth.func@gmail.com>
2025-06-18 22:48:17 -0400

    refactor(logging): stream as arg

--- uot/utils/logging.py
+++ uot/utils/logging.py
@@ -4,11 +4,12 @@ import sys
 
 def setup_logger(
         name: str = "uot",
-        level: int = logging.INFO
+        level: int = logging.INFO,
+        stream=sys.stdout
 ) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
-        ch = logging.StreamHandler(sys.stdout)
+        ch = logging.StreamHandler(stream)
         fmt = logging.Formatter(
             "%(asctime)s %(name)s %(levelname)s: %(message)s")
         ch.setFormatter(fmt)
