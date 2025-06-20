55067fcb9c4ab9413329bab9baa480eb7ddf0f7b
Ivan Zhytkevych <aipyth.func@gmail.com>
2025-06-20 15:10:06 -0400

    fix(emasurement): add cpu and ram

--- uot/experiments/measurement.py
+++ uot/experiments/measurement.py
@@ -27,12 +27,23 @@ def measure_with_gpu_tracker(prob, solver, *args, **kwargs):
     usage = gt.resource_usage
     peak_gpu_ram = usage.max_gpu_ram
     gpu_utilization = usage.gpu_utilization
+    peak_ram = usage.max_ram
+    mean_cpu_pct = usage.cpu_utilization
     metrics.update({
-        'time': usage.compute_time,
-        'gpu_mem_unit': peak_gpu_ram.unit,
-        'peak_gpu_mem': peak_gpu_ram.main,
-        'combined_peak_gpu_ram': peak_gpu_ram.combined,
+        'time_unit': usage.compute_time.unit,
+        'time': usage.compute_time.time,
+
+        'gpu_mem_unit':              peak_gpu_ram.unit,
+        'peak_gpu_mem':              peak_gpu_ram.main,
+        'combined_peak_gpu_ram':     peak_gpu_ram.combined,
+
         'peak_util_pct': gpu_utilization.gpu_percentages.max_hardware_percent,
         'mean_util_pct': gpu_utilization.gpu_percentages.mean_hardware_percent,
+
+        "peak_ram_MiB":              peak_ram.main,
+        "combined_peak_ram_MiB":     peak_ram.combined,
+
+        "max_cpu_util_pct":          mean_cpu_pct.max_hardware_percent,
+        "mean_cpu_util_pct":         mean_cpu_pct.mean_hardware_percent,
     })
     return metrics
