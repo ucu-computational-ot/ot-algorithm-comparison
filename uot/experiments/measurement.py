5fba6b27fb226ba1b3b8f3359c82be606ef9181d
Ivan Zhytkevych <aipyth.func@gmail.com>
2025-06-20 14:24:45 -0400

    fix(measurement): gpu tracker api calls

--- uot/experiments/measurement.py
+++ uot/experiments/measurement.py
@@ -1,6 +1,6 @@
 import jax
 import time
-from gpu_tracker import GPUTracker
+from gpu_tracker.tracker import Tracker
 
 
 def measure_time(prob, solver, marginals, costs, **kwargs):
@@ -12,19 +12,27 @@ def measure_time(prob, solver, marginals, costs, **kwargs):
     return metrics
 
 
-def measure_with_tracker(prob, solver, *args, **kwargs):
-    with GPUTracker() as gt:
+def measure_with_gpu_tracker(prob, solver, *args, **kwargs):
+    with Tracker(
+        sleep_time=0.001,
+        gpu_ram_unit='megabytes',
+        time_unit='seconds',
+    ) as gt:
         metrics = solver().solve(*args, **kwargs)
         # for now drop transport plan and potentials
         metrics.pop('transport_plan', None)
         metrics.pop('u_final', None)
         metrics.pop('v_final', None)
-    stats = gt.get_stats()
+
+    usage = gt.resource_usage
+    peak_gpu_ram = usage.max_gpu_ram
+    gpu_utilization = usage.gpu_utilization
     metrics.update({
-        'time': stats['time_sec'] * 1000,
-        'peak_mem_MiB': stats['max_gpu_ram_MB'],
-        'mean_mem_MiB': stats['mean_gpu_ram_MB'],
-        'peak_util_pct': stats['max_gpu_util_pct'],
-        'mean_util_pct': stats['mean_gpu_util_pct'],
+        'time': usage.compute_time,
+        'gpu_mem_unit': peak_gpu_ram.unit,
+        'peak_gpu_mem': peak_gpu_ram.main,
+        'combined_peak_gpu_ram': peak_gpu_ram.combined,
+        'peak_util_pct': gpu_utilization.gpu_percentages.max_hardware_percent,
+        'mean_util_pct': gpu_utilization.gpu_percentages.mean_hardware_percent,
     })
     return metrics
