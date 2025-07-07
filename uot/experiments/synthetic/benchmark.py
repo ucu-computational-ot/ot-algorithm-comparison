640c1abe89391d86626c0859c5865412ee1d4e27
Ivan Zhytkevych <aipyth.func@gmail.com>
2025-07-07 13:33:36 -0400

    feat(experiment): add online generation of probs

--- uot/experiments/synthetic/benchmark.py
+++ uot/experiments/synthetic/benchmark.py
@@ -6,8 +6,9 @@ from uot.experiments.runner import run_pipeline
 from uot.utils.yaml_helpers import load_solvers, load_problems, load_experiment
 from uot.utils.problems_loaders import load_problems_from_dir
 from uot.problems.hdf5_store import HDF5ProblemStore
-from uot.problems.iterator import ProblemIterator
+from uot.problems.iterator import ProblemIterator, OnlineProblemIterator
 from uot.utils.exceptions import InvalidConfigurationException
+from uot.problems.problem_serializer import _resolve_references
 from uot.utils.logging import setup_logger
 
 logger = setup_logger(__name__)
@@ -22,6 +23,32 @@ def get_problem_iterators(
             return [ProblemIterator(store)]
         return load_problems_from_dir(dataset)
     return load_problems(config=config)
+
+
+def add_online_generators(generator_yaml: str) -> list[ProblemIterator]:
+    with open(generator_yaml, encoding='utf8') as f:
+        raw_cfg = yaml.safe_load(f)
+
+    generators = raw_cfg.get('generators', {})
+    generator_iters = []
+
+    for name, gen_cfg in generators.items():
+        # skip hidden or anchor defaults
+        if name.startswith('_'):
+            continue
+
+        # resolve imports and parsing
+        cfg = {'name': name, **gen_cfg}
+        cfg = _resolve_references(cfg)
+
+        generator_cls = cfg.pop('generator')
+        cache_gt = cfg.pop('cache_gt', False)
+        generator = generator_cls(**cfg)
+        num_datasets = generator._num_datasets
+        generator_iters.append(
+            OnlineProblemIterator(generator, num_datasets, cache_gt)
+        )
+    return generator_iters
 
 
 def main() -> None:
@@ -54,6 +81,14 @@ def main() -> None:
     )
 
     parser.add_argument(
+        "--generators-config",
+        type=str,
+        help="Path to the yaml configuration file of the generators\
+        configuration that will be added to the dataset online (generated\
+        as we run the experiment)."
+    )
+
+    parser.add_argument(
         "--export",
         type=str,
         default="gaussian_toy_results.csv",
@@ -76,6 +111,8 @@ def main() -> None:
         experiment = load_experiment(config=config)
         solver_configs = load_solvers(config=config)
         problems_iterators = get_problem_iterators(args.dataset, config)
+        if args.generators_config:
+            problems_iterators += add_online_generators(args.generators_config)
     except InvalidConfigurationException as ex:
         logger.exception(ex)
         exit(1)
--- uot/problems/iterator.py
+++ uot/problems/iterator.py
@@ -1,6 +1,10 @@
 from collections.abc import Iterator
 from uot.problems.store import ProblemStore
 from uot.problems.base_problem import MarginalProblem
+from uot.problems.problem_generator import ProblemGenerator
+from uot.utils.logging import setup_logger
+
+logger = setup_logger(__name__)
 
 
 class ProblemIterator(Iterator[MarginalProblem]):
@@ -26,3 +30,50 @@ class ProblemIterator(Iterator[MarginalProblem]):
         fn = self._problems[self._idx]
         self._idx += 1
         return self._store.load(fn)
+
+
+class OnlineProblemIterator(Iterator[MarginalProblem]):
+    def __init__(
+            self,
+            generator: ProblemGenerator,
+            num: int,
+            cache_gt: bool = False
+    ):
+        self._num = num
+        self._generator = generator
+        self._idx = 0
+        self._cache_gt = cache_gt
+        self._gen_iterator = self._generator.generate()
+
+    def __iter__(self) -> "OnlineProblemIterator":
+        return self
+
+    def __len__(self) -> int:
+        return self._num
+
+    def __next__(self) -> MarginalProblem:
+        if self._idx >= self._num:
+            raise StopIteration
+        self._idx += 1
+        prob = next(self._gen_iterator)
+        if self._cache_gt:
+            prob.get_costs()
+            prob.get_exact_cost()
+        logger.info(f"Generated problem {prob}")
+        return prob
+
+    def __getstate__(self):
+        # Called by pickle.dumps(self)
+        state = self.__dict__.copy()
+        # remove the actual generator; it's not pickleable
+        state.pop('_gen_iterator', None)
+        return state
+
+    def __setstate__(self, state):
+        # Called by pickle.loads(...)
+        self.__dict__.update(state)
+        # recreate the iterator fresh
+        self._gen_iterator = self._generator.generate()
+        # advance it to where we were
+        for _ in range(self._idx):
+            next(self._iterator)
